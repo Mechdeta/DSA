5b7549b7c5071d4ec1015bf2c2fb4ce9050c890c
Pranav Deshmukh <155602302+Mechdeta@users.noreply.github.com>
2026-04-18 09:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (41.68%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=0;
         for(int i=0;i<nums.length-1;i++){
             if(i!=0 && nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                ans=i;
+                return i;
             }
         }
         if(ans==0){
