df12d7b7e62dd094b81efa7b4e76e1639d96fe15
Pranav Deshmukh <155602302+Mechdeta@users.noreply.github.com>
2026-05-23 12:16:42 +0530

    Time: 572 ms (72.01%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,11 +1,18 @@
-select date_format(trans_date,'%Y-%m') as month,country,Count(*) as trans_count,
-sum(amount) as trans_total_amount,count( case when state="approved" then 1 end)  approved_count, sum(
-    case
-    when state="approved"
-    then amount 
+select date_format(trans_date,'%Y-%m') as month,
+country,
+count(*) as trans_count,
+count(
+    case when state="approved"
+    then 1
+    end
+)as approved_count,
+sum(amount) as trans_total_amount,
+sum(
+    case when state="approved"
+    then amount
     else 0
     end
-) as approved_total_amount
-from transactions 
+)as approved_total_amount
+from transactions
 group by year(trans_date),month(trans_date),country
 order by month
